0f5d99e55d0ae7ca25a87b9aaf896aaefeeda425
Jun Han <formulahendry@gmail.com>
2016-08-14 13:50:55 +0800

    Handle tracking evernt for bat (the executor is '')

--- src/appInsightsClient.ts
+++ src/appInsightsClient.ts
@@ -8,14 +8,14 @@ export class AppInsightsClient {
     private _enableAppInsights;
 
     constructor() {
-        this._client = appInsights.getClient("a25ddf11-20fc-45c6-96ae-524f47754f28");
+        this._client = appInsights.getClient('a25ddf11-20fc-45c6-96ae-524f47754f28');
         let config = vscode.workspace.getConfiguration('code-runner');
         this._enableAppInsights = config.get<boolean>('enableAppInsights');
     }
 
     public sendEvent(eventName: string): void {
         if (this._enableAppInsights) {
-            this._client.trackEvent(eventName);
+            this._client.trackEvent(eventName === '' ? 'bat' : eventName);
         }
     }
 }
