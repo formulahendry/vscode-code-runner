5d0a0bf01675b4ab5a7636487af8fac3dc30c2dd
Cologler <skyoflw@gmail.com>
2018-01-16 16:26:18 +0800

    fixed: output encoding error

--- src/codeManager.ts
+++ src/codeManager.ts
@@ -1,5 +1,6 @@
 "use strict";
 import * as fs from "fs";
+import * as iconv from "iconv-lite";
 import * as os from "os";
 import { dirname, extname, join } from "path";
 import * as vscode from "vscode";
@@ -426,14 +427,19 @@ export class CodeManager implements vscode.Disposable {
         }
         this.sendRunEvent(executor, false);
         const startTime = new Date();
-        this._process = exec(command, { cwd: this._cwd });
+        this._process = exec(command, { cwd: this._cwd, encoding: "Buffer" });
 
-        this._process.stdout.on("data", (data) => {
-            this._outputChannel.append(data);
+        let encoding = this._config.get<string>("outputEncoding");
+        encoding = iconv.encodingExists(encoding) ? encoding : "utf8";
+
+        this._process.stdout.on("data", (buffer) => {
+            const text = iconv.decode(buffer, encoding);
+            this._outputChannel.append(text);
         });
 
-        this._process.stderr.on("data", (data) => {
-            this._outputChannel.append(data);
+        this._process.stderr.on("data", (buffer) => {
+            const text = iconv.decode(buffer, encoding);
+            this._outputChannel.append(text);
         });
 
         this._process.on("close", (code) => {
