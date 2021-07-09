14ce96d030b3ae8f6c92a6f35988ed9d3350559d
Chap Callanan <8684153+ChapC@users.noreply.github.com>
2021-07-10 01:52:02 +1000

    Add stop button in editor title bar (#776)

--- src/codeManager.ts
+++ src/codeManager.ts
@@ -129,6 +129,7 @@ export class CodeManager implements vscode.Disposable {
     private stopRunning() {
         if (this._isRunning) {
             this._isRunning = false;
+            vscode.commands.executeCommand('setContext', 'code-runner.codeRunning', false);
             const kill = require("tree-kill");
             kill(this._process.pid);
         }
@@ -457,6 +458,7 @@ export class CodeManager implements vscode.Disposable {
 
     private async executeCommandInOutputChannel(executor: string, appendFile: boolean = true) {
         this._isRunning = true;
+        vscode.commands.executeCommand('setContext', 'code-runner.codeRunning', true);
         const clearPreviousOutput = this._config.get<boolean>("clearPreviousOutput");
         if (clearPreviousOutput) {
             this._outputChannel.clear();
@@ -482,6 +484,7 @@ export class CodeManager implements vscode.Disposable {
 
         this._process.on("close", (code) => {
             this._isRunning = false;
+            vscode.commands.executeCommand('setContext', 'code-runner.codeRunning', false);
             const endTime = new Date();
             const elapsedTime = (endTime.getTime() - startTime.getTime()) / 1000;
             this._outputChannel.appendLine("");
